65c5751c8636fa17ac1a98094faef7ca00d2f8a3
Rich Boyce <122512687+rboyce212@users.noreply.github.com>
2024-04-02 23:02:07 -0400

    styled the userprofile again from Jackies push plus styled the new goal page and added global font

--- src/Pages/UserProfile.jsx
+++ src/Pages/UserProfile.jsx
@@ -5,6 +5,7 @@ import "./UserProfile.css";
 import Goals from "./Goals";
 import profilePic from "../assets/profile-male-blue.png";
 import { FaPlusCircle } from "react-icons/fa";
+// import { Button2 } from "flowbite-react";
 
 const UserProfile = ({ setUser, setToken, user, token }) => {
   const API = import.meta.env.VITE_BASE_URL;
@@ -47,29 +48,40 @@ const UserProfile = ({ setUser, setToken, user, token }) => {
 
   return (
     <div className="userprofile-container">
-      <img src={profilePic} alt="Profile Image" />
-      <div className="profile-header">
-        <h5>
-          {profiles.firstname} {profiles.lastname}
-        </h5>
-        <h6>{`@${profiles.username}`}</h6>
-        <h6>Age: {profiles.age}</h6>
-        <h6>Gender: {profiles.gender}</h6>
+      <div className="profile-picture">
+        <img id="profile-image" src={profilePic} alt="Profile Image" />
       </div>
-      <div className="css-button-shadow-border--black">
+      <div className="profile-header">
+        <h4>
+          {profiles.firstname} {profiles.lastname}
+        </h4>
+        <h6>
+          {`@${profiles.username}`}
+          <br />
+          <br />
+          Age: {profiles.age}
+          <br />
+          Gender: {profiles.gender}
+        </h6>
+      </div>
+      <br />
+      <div className="logout-button">
         <Button
-          className="css-button-shadow-border--black"
+          className="lg-button"
           onClick={handleLogout}
           style={{ color: "white" }}
         >
           Log Out
         </Button>
       </div>
+
       <div className="bio">
-        <p>
-          Bio: <br />
-          {profiles.bio}
-        </p>
+        <div className="bio-wrapper">
+          <p className="bold" style={{ color: "white" }}>
+            Bio:
+          </p>
+          <p className="ital-bio">{profiles.bio}</p>
+        </div>
       </div>
       <div className="active">
         <button
@@ -87,17 +99,17 @@ const UserProfile = ({ setUser, setToken, user, token }) => {
           Completed
         </button>
       </div>
-      {/* <div className="goals"> */}
+      {/* <div className="profile-goals"> */}
       {!selectedGoals ? (
         <Goals user={user} token={token} />
       ) : (
         <span>List of Completed Goals</span>
       )}
+      {/* </div> */}
       <Link to="/goals/new" className="newgoal-button">
         <FaPlusCircle className="newgoal-icon" />
       </Link>
     </div>
-    // </div>
   );
 };
 
