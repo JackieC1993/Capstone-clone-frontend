767e98bb5c066b8123f4ac1109d93b2486194f33
ShakaPursuit <shakalivngstone@pursuit.org>
2024-04-16 10:37:56 -0400

    trying 3002 after capstone end

--- src/Components/JoinRoom.jsx
+++ src/Components/JoinRoom.jsx
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
 
-const socket = io.connect("https://capstone-backend-ny1d.onrender.com");
+const socket = io.connect("https://capstone-backend-ny1d.onrender.com/3002");
 
 function JoinRoom() {
   const [username, setUsername] = useState("");
