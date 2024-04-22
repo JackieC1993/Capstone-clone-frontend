f01decd3dd81a41ea9c85db2e5323bc87f59f58a
ShakaPursuit <shakalivngstone@pursuit.org>
2024-04-22 13:11:03 -0400

    changed lenght of goal description

--- src/Components/NewGoal.jsx
+++ src/Components/NewGoal.jsx
@@ -106,7 +106,7 @@ const NewGoal = ({ user, token }) => {
           <label htmlFor="description">Goal Description:</label>
           <br />
           <textarea
-            value={`Master python within 10 months starting with basics, learning syntax, data structures, OOP, libraries.Progress will be tracked through completing online courses, tutorials, and monthly projects, with daily dedication to learning and practicing Python. And engage with the community
+            value={`Master python within 10 months starting with basics, learning syntax, data structures, OOP, libraries.Progress will be tracked through completing online courses, tutorials, and monthly projects, with daily dedication to learning and practicing Python. 
             `||goal.description}
             onChange={handleTextChange}
             id="description"
